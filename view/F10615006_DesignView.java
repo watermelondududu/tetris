10304d16fab11b693a7b531aced5a8289de231e6
wehann <wehann@outlook.com>
2018-01-04 23:09:25 +0800

    draw square

--- view/F10615006_DesignView.java
+++ view/F10615006_DesignView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,7 +13,7 @@ import model.shape.Shapes;
 
 
 public class F10615006_DesignView extends JPanel implements ActionListener {
-
+	
 	private controller currentController;
 	
 	public F10615006_DesignView() {
@@ -32,6 +33,31 @@ public class F10615006_DesignView extends JPanel implements ActionListener {
 	
 	// draw this square
 	public void drawSquare(Graphics g, int x, int y, shape.Shapes shape) {
+		//the color of all the shapes
+		Color colors[] = { new Color(0, 0, 0), new Color(204, 102, 102), 
+				new Color(102, 204, 102), new Color(102, 102, 204), 
+				new Color(204, 204, 102), new Color(204, 102, 204),
+				new Color(102, 204, 204), new Color(218, 170, 0)
+		};
+		Color color = colors[shape.ordinal()];
+		
+		final int BOARD_WIDTH = 10;
+		final int BOARD_HEIGHT = 22;
+		
+		int squareWidth = (int)getSize().getWidth() / BOARD_WIDTH;
+		int squareHeight = (int)getSize().getHeight() / BOARD_HEIGHT;
+		
+		// fill the color of the square
+		g.setColor(color);
+		g.fillRect(x + 1, y + 1, squareWidth - 2, squareHeight - 2);
+		
+		// left and top sides of the square are brighter
+		g.setColor(color.brighter());
+		g.drawLine(x, y + squareHeight - 1, x, y);
+		g.drawLine(x, y, x + squareWidth - 1, y);
+		
+		// bottom and right sides of the square are darker
+		// in order to make the square more 3D >_<
 		
 	}
 
