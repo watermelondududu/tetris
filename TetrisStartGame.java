7aab8eb84cad7fa1b22b4540a8a4dff10bf9a6bf
wehann <wehann@outlook.com>
2018-01-07 01:17:00 +0800

    add start game frame

--- TetrisStartGame.java
+++ TetrisStartGame.java
@@ -1,12 +1,34 @@
 package tetris;
 
-import tetris.views.TetrisFrame;
+import view.F10615006_DesignView;
+
+import javax.swing.*;
+import java.awt.*;
 
 
-public class TetrisStartGame {
+public class TetrisStartGame extends JFrame {
+	JLabel status;
+	public TetrisStartGame() {
+		//create a board and start
+		status = new JLabel("0");
+		add(status, BorderLayout.SOUTH);
+		F10615006_DesignView board = new F10615006_DesignView(this);
+		add(board);
+		board.start();
+		
+		//set parameters
+		setSize(200, 400);
+		setTitle("Tetris Game");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public JLabel getStatus() {
+		return status;
+	}
+	
     public static void main(String[] args) {
-        TetrisFrame game = new TetrisFrame();
+        TetrisStartGame game = new TetrisStartGame();
         game.setLocationRelativeTo(null);
-        game.init();
+        game.setVisible(true);
     }
 }
