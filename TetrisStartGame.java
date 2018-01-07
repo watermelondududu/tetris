dc19a1b59fb4bd2cc7f6dc3463dea2c88dc68a9c
wehann <wehann@outlook.com>
2018-01-07 18:45:21 +0800

    revise entrance

--- TetrisStartGame.java
+++ TetrisStartGame.java
@@ -1,34 +1,16 @@
 package tetris;
 
+import view.DesignView;
+import view.ShowView;
 import view.F10615006_DesignView;
 
 import javax.swing.*;
 import java.awt.*;
 
-
 public class TetrisStartGame extends JFrame {
-	JLabel status;
-	public TetrisStartGame() {
-		//create a board and start
-		status = new JLabel("0");
-		add(status, BorderLayout.SOUTH);
-		F10615006_DesignView board = new F10615006_DesignView(this);
-		add(board);
-		board.start();
-		
-		//set parameters
-		setSize(200, 400);
-		setTitle("Tetris Game");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-	
-	public JLabel getStatus() {
-		return status;
-	}
-	
     public static void main(String[] args) {
-        TetrisStartGame game = new TetrisStartGame();
+        DesignView game = new F10615006_DesignView();
         game.setLocationRelativeTo(null);
-        game.setVisible(true);
+        game.init();
     }
 }
