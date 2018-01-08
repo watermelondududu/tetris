f1cd29b898b1e300fa2fd9c0d5853d51fc2096c4
AlexMeow <oo587868@hotmail.com>
2018-01-09 01:22:03 +0800

    update

--- tetris/TetrisStartGame.java
+++ tetris/TetrisStartGame.java
@@ -3,13 +3,15 @@ package tetris;
 import view.DesignView;
 import view.ShowView;
 import view.F10615006_DesignView;
+import view.B10413040_DesignView;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class TetrisStartGame extends JFrame {
     public static void main(String[] args) {
-        DesignView game = new F10615006_DesignView();
+    	DesignView game = new F10615006_DesignView();
+        //DesignView game = new B10413040_DesignView();
         game.setLocationRelativeTo(null);
         game.init();
     }
