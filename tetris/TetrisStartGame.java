02b33eb17d8ea489f830c972d68cc2d46828246f
wehann <wehann@outlook.com>
2018-01-11 15:58:53 +0800

    F10615006

--- tetris/TetrisStartGame.java
+++ tetris/TetrisStartGame.java
@@ -5,16 +5,17 @@ import view.ShowView;
 import view.F10615006_DesignView;
 import view.F10615007_DesignView;
 import view.B10413040_DesignView;
+import view.B10415034_DesignView;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class TetrisStartGame extends JFrame {
     public static void main(String[] args) {
-//    	DesignView game = new F10615006_DesignView();
- //      DesignView game = new F10615007_DesignView();
-  //    DesignView game = new B10415034_DesignView();
-        DesignView game = new B10413040_DesignView();
+    		DesignView game = new F10615006_DesignView();
+//    		DesignView game = new F10615007_DesignView();
+//    		DesignView game = new B10415034_DesignView();
+//    		DesignView game = new B10413040_DesignView();
         game.setLocationRelativeTo(null);
         game.init();
     }
--- view/F10615006_DesignView.java
+++ view/F10615006_DesignView.java
@@ -31,16 +31,18 @@ public class F10615006_DesignView extends JFrame implements DesignView {
         
         add(View);
         View.pause();
+        View.setBackground(Color.BLACK);
 
         //panel1 contains a status bar
         JPanel panel1 = new JPanel();
         panel1.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-        panel1.setBackground(Color.ORANGE);
+        status.setFont(new Font("Times New Roman", 1, 20));
+        panel1.setBackground(Color.white);
         panel1.add(status, BorderLayout.NORTH);
         contentPane.add(panel1, BorderLayout.NORTH);
         
         //panel2 contains start & pause button
-        JPanel panel2 = new JPanel(new GridLayout(1, 2));
+        JPanel panel2 = new JPanel(new GridLayout(1, 3));
         panel2.setBorder(BorderFactory.createEtchedBorder());
         
         JButton startbutton = new JButton("Restart");
@@ -53,13 +55,18 @@ public class F10615006_DesignView extends JFrame implements DesignView {
         pausebutton.addActionListener(new PauseButtonHandler());
         pausebutton.addKeyListener(new MyKeyAdapter(currentController));
         
+        JButton exitbutton = new JButton("Exit");
+        panel2.add(exitbutton);
+        exitbutton.addActionListener(new ExitButtonHandler());
+        exitbutton.addKeyListener(new MyKeyAdapter(currentController));
+        
         contentPane.add(panel2, BorderLayout.SOUTH);
         
         //panel3 contains operations instructions
         JPanel panel3 = new JPanel(new GridLayout(20, 1));
         panel3.setPreferredSize(new Dimension(200, 450));
         panel3.setBorder(BorderFactory.createEtchedBorder());
-        panel3.setBackground(Color.CYAN);
+        panel3.setBackground(Color.lightGray);
         JLabel label0 = new JLabel();
         JLabel label1 = new JLabel("  Instructions" + '\n');
         label1.setFont(new Font("Dialog", 1, 20));
@@ -67,14 +74,16 @@ public class F10615006_DesignView extends JFrame implements DesignView {
         label2.setFont(new Font("Times New Roman", 0, 15));
         JLabel label3 = new JLabel("2. Drop: D/d" + '\n');
         label3.setFont(new Font("Times New Roman", 0, 15));
-        JLabel label4 = new JLabel("3. Rotate: KEY_Up" + '\n');
+        JLabel label4 = new JLabel("3. Restart: R/r" + '\n');
         label4.setFont(new Font("Times New Roman", 0, 15));
-        JLabel label5 = new JLabel("4. Move Left: KEY_Left" + '\n');
+        JLabel label5 = new JLabel("4. Rotate: KEY_Up" + '\n');
         label5.setFont(new Font("Times New Roman", 0, 15));
-        JLabel label6 = new JLabel("5. Move Right: KEY_Right" + '\n');
+        JLabel label6 = new JLabel("5. Move Left: KEY_Left" + '\n');
         label6.setFont(new Font("Times New Roman", 0, 15));
-        JLabel label7 = new JLabel("6. One line down: KEY_Down");
+        JLabel label7 = new JLabel("6. Move Right: KEY_Right" + '\n');
         label7.setFont(new Font("Times New Roman", 0, 15));
+        JLabel label8 = new JLabel("7. One line down: KEY_Down");
+        label8.setFont(new Font("Times New Roman", 0, 15));
         panel3.add(label1);
         panel3.add(label0);
         panel3.add(label2);
@@ -83,6 +92,7 @@ public class F10615006_DesignView extends JFrame implements DesignView {
         panel3.add(label5);
         panel3.add(label6);
         panel3.add(label7);
+        panel3.add(label8);
         contentPane.add(panel3, BorderLayout.EAST);
         
         setSize(500, 650);
@@ -108,6 +118,12 @@ public class F10615006_DesignView extends JFrame implements DesignView {
                         currentController.pause();
         }
         }
+        
+        private class ExitButtonHandler implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                    System.exit(0);
+            }
+            }
         
         public Color[] getColors() {
     		Color tmp[] = { new Color(0, 0, 0), new Color(204, 102, 102), 
